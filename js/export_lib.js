8cab15330593093c6bc0173efa71b42d449206cd
paulo1er <paulmadeline@orange.fr>
2018-03-21 15:25:28 +0000

    not in {}

--- js/export_lib.js
+++ js/export_lib.js
@@ -430,7 +430,7 @@ var exportLib = (function() {
 					{
 						output = output + "\\page";
 					}
-					if ((note !== "") && options.outputNotes) output = output + "\n" + indent  + note ;
+					if ((note !== "") && options.outputNotes) output = output + "\n" + indent  +"{" note +"}";
 					output = output + "\n";
 				}
 				else {
