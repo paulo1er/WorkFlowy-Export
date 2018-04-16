be121abff044e94393f45abcfe9f19d070c6ddd0
paulo1er <paulmadeline@orange.fr>
2018-04-16 13:17:03 +0100

    Can refreh Import from WorkFlowy

--- js/popup.js
+++ js/popup.js
@@ -1,18 +1,15 @@
 (function() {
 
-	chrome.tabs.query({
-		active: true,
-		currentWindow: true
-	}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, {
-			request: 'getTopic'
-		}, function(response) {
-			var a = window.open("popup2.html", "_blank", "location=1,status=1,scrollbars=1, width=500,height=500");
-			a.focus();
+			chrome.tabs.query({
+				active: true,
+				currentWindow: true
+			}, function(tabs) {
+					var a = window.open("popup2.html", "_blank", "location=1,status=1,scrollbars=1, width=500,height=500");
+					a.focus();
 
-			a.addEventListener('load', function(){
-				a.popup2.main(response);
-			}, true);
-		});
-	});
+					a.addEventListener('load', function(){
+						a.popup2.main(tabs[0].id);
+					}, true);
+			});
+
 }());
